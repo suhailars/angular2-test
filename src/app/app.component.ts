fa4c1593083daaa2eed2dc7ffe189bfc1696144b
suhail <suhail.ars@gmail.com>
2017-08-04 18:59:43 +0530

    added new link about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component }          from '@angular/core';
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
       <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/about" routerLinkActive="active">About</a>
     </nav>
     <router-outlet></router-outlet>
   `,
